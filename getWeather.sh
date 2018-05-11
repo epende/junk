9716f0d76cd4e1b1df6a4e426e096578b26be763
Eric Pendergrass <epende@gmail.com>
2018-05-11 20:28:59 +0000

    another weather script

---
+++ getWeather.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+name=unset
+value=unset
+if [[ $# -eq 0 ]] ; then
+    echo 'USAGE:  $0 <IP>'
+    exit 0
+fi
+
+IP=$1
+
+IFS=$'\n'
+for f in $(wget http://${IP}/livedata.htm -O - | grep "input name" | grep -v "efault" | grep -i "temp\|hum\|wind\|press\|gust\|rain" | awk -F'"' '{print $4" = "$14}'); 
+do 
+    echo $f
+done
