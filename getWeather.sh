9e048a772e238a6abd8bdb9a0c4016349a03f9ae
Eric Pendergrass <epende@gmail.com>
2018-05-12 17:05:05 +0000

    Fixed wind errors

--- getWeather.sh
+++ getWeather.sh
@@ -10,45 +10,48 @@ STATION_ID="KCOFORTC310"
 WIND_SPEED_THRESHOLD=20
 WIND_GUST_THRESHOLD=30
 
-IFS=$'\n'
-for f in $(wget http://${IP}/livedata.htm -O - | grep "input name" | grep -v "efault" | grep -i "temp\|hum\|wind\|press\|gust\|rain" | awk -F'"' '{print $4"="$14}'); 
-do 
-    val=$(echo $f | grep -i outhumi | awk -F"=" '{print $2}')
+f=$(wget http://${IP}/livedata.htm -O - | grep "input name" | grep -v "efault" | grep -i "temp\|hum\|wind\|press\|gust\|rain" | awk -F'"' '{print $4"="$14}')
+echo $line
+#IFS=$'\n'
+#for f in $(wget http://${IP}/livedata.htm -O - | grep "input name" | grep -v "efault" | grep -i "temp\|hum\|wind\|press\|gust\|rain" | awk -F'"' '{print $4"="$14}'); 
+#do 
+    val=$(echo "$f" | grep -i outhumi | awk -F"=" '{print $2}')
     if [ ! -z $val ];
     then
         humidity=$val
     fi
-    val=$(echo $f | grep -i outTemp | awk -F"=" '{print $2}')
+    val=$(echo "$f" | grep -i outTemp | awk -F"=" '{print $2}')
     if [ ! -z $val ];
     then
         tempf=$val
     fi
-    val=$(echo $f | grep -i outHumi | awk -F"=" '{print $2}')
+    val=$(echo "$f" | grep -i outHumi | awk -F"=" '{print $2}')
     if [ ! -z $val ];
     then
         outhumi=$val
     fi
-    val=$(echo $f | grep -i abspres | awk -F"=" '{print $2}')
+    val=$(echo "$f" | grep -i abspres | awk -F"=" '{print $2}')
     if [ ! -z $val ];
     then
         baromin=$val
     fi
-    val=$(echo $f | grep -i windir | awk -F"=" '{print $2}')
+    val=$(echo "$f" | grep -i windir | awk -F"=" '{print $2}')
     if [ ! -z $val ];
     then
         winddir=$val
     fi
-    val=$(echo $f | grep -i avgwind | awk -F"=" '{print $2}')
+    val=$(echo "$f" | grep -i avgwind | awk -F"=" '{print $2}')
+    echo "avgwind: $val"
     if [ ! -z $val ];
     then
         windspeedmph=$val
     fi
-    val=$(echo $f | grep -i gustspeed | awk -F"=" '{print $2}')
+    val=$(echo "$f" | grep -i gustspeed | awk -F"=" '{print $2}')
     if [ ! -z $val ];
     then
         windgustmph=$val
     fi
-done
+#done
 
 echo "humidity: $humidity"
 echo "tempf: $tempf"
