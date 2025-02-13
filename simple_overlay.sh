5653843564fc4ce94182895551b1a0d328931326
Eric Pendergrass <epende@gmail.com>
2025-02-12 17:23:52 -0700

    add file param

--- simple_overlay.sh
+++ simple_overlay.sh
@@ -5,4 +5,8 @@
 OVR_STRING=$(/home/pendergr/wx_scraper/collect.py -i wx.dammitly.net | jq | tr -d '"')
 echo $OVR_STRING
 
+if [ $# -gt 1 ]'
+then
+	OVR_STRING=$(cat $2)
+fi
 mogrify -fill white -font Courier -undercolor '#00000080' -pointsize 42 -gravity SouthWest -annotate +10+10 "$OVR_STRING" $1
